fe850419f6f7721011121a146148511fb304b409
Emil Abraham <emilabraham1414@gmail.com>
2018-08-01 21:36:15 -0400

    gitconfig as well

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfilesOld             # old dotfiles backup directory
-files="bashrc vimrc bash_profile vim tmux.conf git-completion.bash"    # list of files/folders to symlink in homedir
+files="bashrc vimrc bash_profile vim tmux.conf git-completion.bash gitconfig"    # list of files/folders to symlink in homedir
 
 ##########
 
